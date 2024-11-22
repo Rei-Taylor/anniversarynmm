402832321f42d60ee593dab6cacceeedf1268e1f
Rei-Taylor <120787198+Rei-Taylor@users.noreply.github.com>
2024-11-22 16:47:28 +0630

    Update annive.py

--- annive.py
+++ annive.py
@@ -27,6 +27,7 @@ img2 = r"c.jpg"
 img3 = r"abc.jpg"
 img4 = r"def.jpg"
 img5 = r"hij.jpg"
+img6 = r"cha.jpg"
 cols = st.columns([1,3,1])
     
 with cols[1]:
@@ -47,9 +48,9 @@ with st.container(border=True):
 with st.container(border=True):
     cols = st.columns([1,1,1])
     with cols[0]:
-        st.image(img2)
+        st.image(img5)
     with cols[1]:
-        st.image(img3)
+        st.image(img6)
     with cols[2]:
         st.image(img4)
 
